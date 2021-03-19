b884b2e153f02f4e0b9f915ad4db650921e7ab1e
melmo <mail@melaniethewlis.com>
2021-03-19 23:35:24 +1100

    tidy up database connection ENV

--- app/models/index.js
+++ app/models/index.js
@@ -1,8 +1,11 @@
 const Sequelize = require("sequelize"); 
 const sllSettings = process.env.NODE_ENV != "development" ? { require:true, rejectUnauthorized:false} : false;
 
-const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+//const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  //host: process.env.DB_HOST,
   dialect: 'postgres',
   dialectOptions: {
     ssl: sllSettings
