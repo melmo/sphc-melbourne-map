431672c5aee98631b2b8561e5b293a6e8e388163
melmo <mail@melaniethewlis.com>
2021-03-19 23:05:54 +1100

    ssl database connection

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,12 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
-
+  dialectOptions: {
+      ssl: {      /* <----- Add SSL option */
+        require: true,
+        rejectUnauthorized: false 
+      }
+    }
   pool: {
     max: 5,
     min: 0,
