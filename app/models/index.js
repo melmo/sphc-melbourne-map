ea3119398653fb171f9335eb3265dbe917ea00ed
melmo <mail@melaniethewlis.com>
2021-03-19 22:54:32 +1100

    attempt reprovision database

--- app/models/index.js
+++ app/models/index.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize"); 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DATABASE_URL,
   dialect: 'postgres',
